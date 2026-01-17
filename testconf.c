fabef5b0e9c403ee41f5c73030086d95df34493b
hana-zain <hanazain416@gmail.com>
2026-01-17 12:02:51 +0530

    added

--- testconf.c
+++ testconf.c
@@ -4,7 +4,7 @@ int main() {
     int sum = 0;  // Variable to store the sum
 
     // Loop from 1 to 10
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 100; i++) {
         sum += i;  // Add i to sum
     }
 
