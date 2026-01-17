ac1983f561c011149ac9463affb5180f54166d2b
shyma <shymazuhra22@gmail.com>
2026-01-17 12:02:16 +0530

    C file

---
+++ testconf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;  // Variable to store the sum
+
+    // Loop from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        sum += i;  // Add i to sum
+    }
+
+    // Print the result
+    printf("The sum of numbers from 1 to 10 is: %d\n", sum);
+
+    return 0;
+}
